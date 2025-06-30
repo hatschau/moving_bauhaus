db0b1175eb32860285089d2c8a9fbfac6cdba106
Helena <helenakoester@gmx.de>
2025-06-30 15:27:58 +0200

    Play all animations, not just the first one

--- src/components/ModelViewer.jsx
+++ src/components/ModelViewer.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect } from 'react'
 import { useGLTF, useAnimations } from '@react-three/drei'
 
-export default function ModelViewer({ url }) {
+export default function ModelViewer({ url, ...props }) {
   const gltf = useGLTF(url)
   const { scene, animations } = gltf
   const { actions } = useAnimations(animations, scene)
 
   useEffect(() => {
     if (actions && animations.length > 0) {
-      actions[animations[0].name]?.play()
+      Object.values(actions).forEach(action => {
+        action.reset().play()
+      })
     }
   }, [actions, animations])
 
-  return <primitive object={scene} />
+  return <primitive object={scene} {...props} />
 }
